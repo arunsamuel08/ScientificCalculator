d3086e58d54e8921218e1f7a9fd2bab055ea5aba
arunsamuel08 <arunsamuel08@users.noreply.github.com>
2020-12-13 12:28:33 +0530

    commit

---
+++ Scientific calculator.py
@@ -0,0 +1,19 @@
+# importing modules
+import tkinter as tk
+#from tkinter import messagebox
+#import math as m
+
+class CalculatorWindow:
+    def __init__(self, master):
+        self.master = master
+        # adding title name
+        master.title('Scientific Calculator')
+        # setting the size 
+        master.geometry('500x500')
+        # disabling the resizing of the window
+        master.resizable(0.0)
+
+if __name__ == '__main__':
+master = tk.Tk()
+app = CalculatorWindow(master)
+master.mainloop()
