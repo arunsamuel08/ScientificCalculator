e315e9c7fece5a10647d4cbb34c5d895e83ee310
arunsamuel08 <arunsamuel08@users.noreply.github.com>
2020-12-14 23:22:55 +0530

    commit

--- Scientific calculator.py
+++ Scientific calculator.py
@@ -1,5 +1,5 @@
 # importing modules
-import tkinter as tk
+from tkinter import*
 #from tkinter import messagebox
 #import math as m
 
@@ -9,17 +9,57 @@ class CalculatorWindow:
         # adding title name
         master.title('Scientific Calculator')
         # setting the size 
-        master.geometry('500x500+0+0')
+        master.geometry('480x568+0+0')
         # disabling the resizing of the window
         master.resizable(0,0)
         # giving background colour
         master.configure(background = "black")
+        
+        # adding frame to the calculator 
+        calc = Frame(master)
+        calc.grid()
+
+        # making menus
+        menubar = Menu(calc)
+
+        # added file menu in which we will toggle between the two calcs
+        filemenu = Menu(menubar, tearoff = 0)
+        menubar.add_cascade(label = "File", menu=filemenu)
+        # added the toggle button for standard calc 
+        filemenu.add_command(label = "Standard")
+        # added the toggle button for scientific calc
+        filemenu.add_command(label = "Scientific")
+        # added in a separator
+        filemenu.add_separator()
+        # added a exit option.
+        filemenu.add_command(label = "Exit")
+
+        # command used to call this file menu
+        master.config(menu=menubar)
+
+        # adding an edit menu from which we can cut, copy or paste
+        editmenu = Menu(menubar, tearoff = 0)
+        menubar.add_cascade(label = "Edit", menu=editmenu)
+        # added button for cut, copy and past
+        editmenu.add_command(label = "Cut")
+        editmenu.add_command(label = "Copy")
+        editmenu.add_command(label = "Paste")
+
+        # command to call this edit menu
+        master.config(menu=menubar)
+        # added a button to view help
+        helpmenu = Menu(menubar, tearoff=0)
+        menubar.add_cascade(label = "Help", menu=helpmenu)
+        helpmenu.add_command(label = "View Help")
+
+        master.config(menu=menubar)
 
         
 
 
+
 if __name__ == '__main__':
-    master = tk.Tk()
+    master = Tk()
     app = CalculatorWindow(master)
     master.mainloop()
     
